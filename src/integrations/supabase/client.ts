4f2d0a7fa7fb947734dc0f80b1c3d5949cd19f36
Lusmaysh <89325163+Lusmaysh@users.noreply.github.com>
2025-08-04 21:26:19 +0700

    add .env and gitignore for sensitive data

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ftwykhmauasmxpygshnf.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0d3lraG1hdWFzbXhweWdzaG5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NjE4MjUsImV4cCI6MjA2NzMzNzgyNX0.-py2GYp-lSSc-Iz2iIsrgSrZpCHM7zubAaTZWt-bQfM";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
